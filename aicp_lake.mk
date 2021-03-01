91c7ce8d9f0d57187920052d29eb918955f0696c
Ali B <abittin@gmail.com>
2021-03-01 14:34:47 +0300

    lake: : override product info for safetynet
    * Plus: use walleye oreo fingerprint, since lake fp is borked xD
    
    Change-Id: I9cd28522330b18a52ecc469c3f5da37c0f260f18

--- aicp_lake.mk
+++ aicp_lake.mk
@@ -65,6 +65,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := motorola/lake_retail/lake:10/QPWS30.61-21-18-7-12/b1002:user/release-keys
 
+PRODUCT_OVERRIDE_INFO := true
+PRODUCT_OVERRIDE_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Jeferson Oliveira (jro1979oliver)"
